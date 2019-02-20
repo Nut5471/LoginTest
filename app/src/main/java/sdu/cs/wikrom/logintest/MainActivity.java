407142c6e1f4dac4017aadd7dbc632cab5695edb
Nut5471 <wikromkongnaem@gmail.com>
2019-02-20 12:02:35 +0700

    UI complete

---
+++ app/src/main/java/sdu/cs/wikrom/logintest/MainActivity.java
@@ -0,0 +1,14 @@
+package sdu.cs.wikrom.logintest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }//end onCreate
+
+}//end class
